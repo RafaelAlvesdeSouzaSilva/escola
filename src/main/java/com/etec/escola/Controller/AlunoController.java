e8463ed84d40f4a66381669ced1f034382754ee1
RafaelAlvesdeSouzaSilva <rafinhaalvesss70@gmail.com>
2025-08-13 17:20:27 -0300

    t

--- src/main/java/com/etec/escola/Controller/AlunoController.java
+++ src/main/java/com/etec/escola/Controller/AlunoController.java
@@ -1,4 +1,11 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Service.AlunoService;
+
 public class AlunoController {
+    private final AlunoService as;
+
+    public AlunoController(AlunoService as) {
+        this.as = as;
+    }
 }
